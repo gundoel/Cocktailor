f5be809478bff10e913b6e554e33f191084f5c30
rebsamench <rebsamen.ch@bluewin.ch>
2020-08-30 15:12:33 +0200

    My Bar Layout neu

---
+++ app/src/main/java/ch/zhaw/ma20/cocktailor/adapters/MyBarListAdapter.kt
@@ -0,0 +1,52 @@
+package ch.zhaw.ma20.cocktailor.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import ch.zhaw.ma20.cocktailor.model.IngredientListItem
+import com.example.cocktailor.R
+import kotlinx.android.synthetic.main.ingredient_selectable_item.view.*
+
+class MyBarListAdapter(
+    var ingredientsList: MutableList<IngredientListItem>,
+    val context: Context
+) : BaseAdapter() {
+
+    var layoutInflater: LayoutInflater
+
+    init {
+        // sort alphabetically
+        ingredientsList.sortBy { it.strIngredient1.toString() }
+        layoutInflater = LayoutInflater.from(context)
+    }
+
+    override fun getCount(): Int { //number of elements to display
+        return ingredientsList.size
+    }
+
+    override fun getItem(index: Int): IngredientListItem { //item at index
+        return ingredientsList.get(index)
+    }
+
+    override fun getItemId(index: Int): Long { //itemId for
+        return 0
+    }
+
+    override fun getView(
+        index: Int, oldView: View?,
+        viewGroup: ViewGroup?
+    ): View {
+        var view: View
+        if (oldView == null) { //check if we get a view to recycle
+            view = layoutInflater.inflate(R.layout.ingredient_item, null)
+        } else { //if yes, use the oldview
+            view = oldView
+        }
+
+        val entry = getItem(index) //get the data for this index
+        view.ingredientName.text = entry.strIngredient1 //and fill the layout
+        return view
+    }
+}
--- app/src/main/java/ch/zhaw/ma20/cocktailor/fragments/MyBarFragment.kt
+++ app/src/main/java/ch/zhaw/ma20/cocktailor/fragments/MyBarFragment.kt
@@ -5,15 +5,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import ch.zhaw.ma20.cocktailor.adapters.IngredientsSearchAdapter
+import ch.zhaw.ma20.cocktailor.adapters.MyBarListAdapter
+import ch.zhaw.ma20.cocktailor.model.IngredientListItem
+import ch.zhaw.ma20.cocktailor.model.RemoteDataCache
 import com.example.cocktailor.R
+import kotlinx.android.synthetic.main.fragment_finder.view.*
+import kotlinx.android.synthetic.main.fragment_my_bar.view.*
 
 class MyBarFragment : Fragment() {
+    var adapter: MyBarListAdapter? = null;
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_my_bar, container, false)
+        var layout = inflater.inflate(R.layout.fragment_my_bar, container, false)
+        adapter = MyBarListAdapter(RemoteDataCache!!.ingredientsList, requireContext())
+        layout.listViewMyBarSearch.adapter = adapter
+        return layout
     }
 }
