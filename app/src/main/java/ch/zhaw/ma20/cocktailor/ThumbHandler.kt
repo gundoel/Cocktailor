502b88d9cf3d04eab84e368fca3de780e2a0ccee
Simon Rizzi <rizzisim@gmail.com>
2020-09-08 19:07:27 +0200

    Made ThumbHandler abstract and added call to FinderFragment

--- app/src/main/java/ch/zhaw/ma20/cocktailor/ThumbHandler.kt
+++ app/src/main/java/ch/zhaw/ma20/cocktailor/ThumbHandler.kt
@@ -6,22 +6,25 @@ import ch.zhaw.ma20.cocktailor.model.Cocktail
 import java.io.File
 
 class ThumbHandler {
-    val service = ServiceVolley()
-    val apiController = APIController(service)
 
-    fun storeSingleThumb(cocktail: Cocktail) {
-        var thumbName = cocktail.idDrink + ".jpg"
-        if (!(File(thumbName).exists())) {
-            var picturePath = cocktail.strDrinkThumb + "/preview"
-            apiController.get(picturePath) {
-                File(Cocktailor.applicationContext().filesDir, thumbName)
+    companion object {
+        val service = ServiceVolley()
+        val apiController = APIController(service)
+        fun storeSingleThumb(cocktail: Cocktail) {
+            var thumbName = cocktail.idDrink + ".jpg"
+            if (!(File(thumbName).exists())) {
+                var picturePath = cocktail.strDrinkThumb + "/preview"
+                apiController.get(picturePath) {
+                    File(Cocktailor.applicationContext().filesDir, thumbName)
+                }
+            }
+        }
+
+        fun storeMultipleThumbs(cocktailList: MutableList<Cocktail>) {
+            for (cocktail in cocktailList) {
+                storeSingleThumb(cocktail)
             }
         }
     }
 
-    fun storeMultipleThumbs(cocktailList: MutableList<Cocktail>) {
-        for (cocktail in cocktailList) {
-            storeSingleThumb(cocktail)
-        }
-    }
 }
--- app/src/main/java/ch/zhaw/ma20/cocktailor/fragments/FinderFragment.kt
+++ app/src/main/java/ch/zhaw/ma20/cocktailor/fragments/FinderFragment.kt
@@ -14,6 +14,7 @@ import androidx.fragment.app.Fragment
 import ch.zhaw.ma20.cocktailor.Cocktailor
 import ch.zhaw.ma20.cocktailor.MainActivity
 import ch.zhaw.ma20.cocktailor.R
+import ch.zhaw.ma20.cocktailor.ThumbHandler
 import ch.zhaw.ma20.cocktailor.adapters.IngredientsSearchAdapter
 import ch.zhaw.ma20.cocktailor.appconst.Connector
 import ch.zhaw.ma20.cocktailor.model.*
@@ -125,6 +126,7 @@ class FinderFragment : Fragment() {
                         else {
                             cocktailList.addAll(cocktailResultCart.getCocktailsAND())
                         }
+                        ThumbHandler.storeMultipleThumbs(cocktailList)
                         // cache cocktails
                         RemoteDataCache.addLastCocktailSearchResult(cocktailList)
                         if (cocktailList.isEmpty()) {
